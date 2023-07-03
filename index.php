5fd2c9e1810be3d2562168ee373978763ba9d86b
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-03 15:17:28 -0500

    feat: :sparkles: Salida printf
    Similar a echo pero solo muestra una cadena de texto a la vez

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-    echo "Hola mundo cruel";
+    /**
+     * *print()
+     * ? Esta funcion es similar a echo(), pero solo puede imprimir una cadena de texto a la vez
+     */
+    $texto = "Mundo";
+    printf("Hola mundo %s", $texto);
 ?>
