10cb80dff515b7efeee6366eb321ace9b364d885
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-03 15:20:36 -0500

    feat: :sparkles: Salida sprintf
    Similar a printf, pero en lugar de imprimir la cadena formateada en la salida estandar devuelve la cadena formateada en la salida estandar
    
    devuelve la cadena formateada como resultado

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
     /**
-     * *print()
-     * ? Esta funcion es similar a echo(), pero solo puede imprimir una cadena de texto a la vez
+     * *sprintf()
+     * ? Esta función es similar a printf(), pero en lugar de imprimir la cadena formateada en la salida estándar, devuelve la cadena formateada como resultado.
      */
     $texto = "Mundo";
-    printf("Hola mundo %s", $texto);
+    $mensaje = sprintf("Hola %s", $texto);
+    echo $mensaje;
 ?>
